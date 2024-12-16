e930fa23bdafef1babfd8518f9d22f50eee13b69
pykretexd <landysixten@gmail.com>
2024-12-17 00:00:33 +0100

    Update main.py

--- src/cython_test/main.py
+++ src/cython_test/main.py
@@ -6,13 +6,13 @@ from py_prime_finder import py_prime_finder
 def main():
     n = 10000
 
-    c_start: float = perf_counter()
-    result = c_prime_finder(n)
-    c_end: float = perf_counter()
+    c_start = perf_counter()
+    c_prime_finder(n)
+    c_end = perf_counter()
 
-    py_start: float = perf_counter()
-    result = py_prime_finder(n)
-    py_end: float = perf_counter()
+    py_start = perf_counter()
+    py_prime_finder(n)
+    py_end = perf_counter()
 
     print('Time elapsed (C):', c_end-c_start)
     print('Time elapsed (Python):', py_end-py_start)
